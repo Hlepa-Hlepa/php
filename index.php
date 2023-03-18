3bb4d3cc5fbf25f2b701ba38885dccafdb8ab03d
Hlepa-Hlepa <kot101kot@inbox.ru>
2023-03-19 01:34:48 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="nav">
+            <img class="logo" src="logo.png">
+            <h1>Лабораторная работа 1 - Тулин Константин 221-323</h1>
+        </div>
+    </header>
+    <main>
+        <div class="cont">
+            <?php 
+            echo 'Hello, World!';
+            ?>
+        </div>
+    </main>
+    <footer>
+        <p>Задание для самостоятельно работы</p>
+    </footer>
+</body>
+</html>
