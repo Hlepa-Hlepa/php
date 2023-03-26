f264538a70262f5cd24be9d93716d777b35b7241
Hlepa-Hlepa <kot101kot@inbox.ru>
2023-03-26 19:12:09 +0300

    Дз №1

---
+++ lab_php1.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="normalize.css">
+    <title>lab_php</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="nav_lvl_1">
+                 <img class="logo" src="top_sh.png">
+                 <p class="name_student">Тулин Константин Александрович 221-323</p>
+            </div>
+            <div class="nav_lvl_2">
+                <a href="index.html">Главная</a>
+                <a href="index.html">lab 1</a>
+                <a href="">lab 2</a>
+                <a href="">lab 3</a>
+                <a href="">lab 4</a>
+                <a href="">lab 5</a>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <h1>2.1.Домашняя работа: Hello, World!</h1>
+        <?php 
+        echo 'Hello, World!';
+        ?>
+    </main>
+    <footer>
+        <p>Задание для самостоятельно работы</p>
+    </footer>
+</body>
+</html>
